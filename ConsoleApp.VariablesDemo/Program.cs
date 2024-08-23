6861a6dedb31be3a589c4cc41f20f79060edf953
Dev <Dev@SERVER3>
2024-08-23 13:07:40 -0700

    Agregar archivos de proyecto.

---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Difference datatypes
+/*
+    text - string
+    integers - int
+    decimal - double, float, decimal
+    logical - bool
+ */
+
+string name = "James Doe";
