9f7dea3f234f846d99d8b961e5d2ebe9f7e00a7f
sistemasmesabi <sistemas@mesabi.com.mx>
2024-08-23 16:16:24 -0700

    Added UserInputDemo

---
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Declare variables
+using System.Xml.Linq;
+
+string firstName = string.Empty;
+string lastName = string.Empty;
+int age = 0;
+int retirementAge = 65;
+
+// Prompt the user for input
+Console.WriteLine("Please enter your first name");
+firstName = Console.ReadLine();
+
+Console.WriteLine("Please enter your last name");
+lastName = Console.ReadLine();
+
+Console.WriteLine("Please enter your age");
+age = Convert.ToInt32(Console.ReadLine());
+
+// Process the data
+int workingYearsRemaning = retirementAge - age;
+
+// Output the results to the user
+Console.WriteLine($"Full name: {firstName} {lastName}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Working years remaining: {workingYearsRemaning}");
