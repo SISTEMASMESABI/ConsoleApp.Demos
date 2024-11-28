274a34e59cb0abfcb3b75e111698f924dfcbe06a
sistemasmesabi <sistemas@mesabi.com.mx>
2024-11-28 09:44:23 -0700

    Added Arrays and Lists demos

---
+++ ConsoleApp.Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Declare Fized Size Array
+int[] grades = new int[5];
+
+// Add values to Fixed Size Array
+/*
+grades[0] = 45;
+grades[1] = 25;
+grades[2] = 38;
+grades[3] = 45;
+grades[4] = 54;
+*/
+for (int i = 0; i < grades.Length; i++)
+{
+    Console.Write("Enter Grade: ");
+    grades[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+// Print values to Fixed Size Array
+Console.WriteLine("The Grades you have entered are: ");
+for (int i = 0; i < grades.Length; i++)
+{
+    Console.Write(grades[i]);
+}
+
+// Declare Variable Size Array
+string[] studentNames = {"Newton", "Joshua", "etc..."};
+
+// Add values to Variable Size Array
+for (int i = 0; i < studentNames.Length; i++)
+{
+    Console.Write("Enter Student Name: ");
+    studentNames[i] = Console.ReadLine();
+}
+
+// Print values to Variable Size Array
+Console.WriteLine("The student names you have entered are: ");
+for (int i = 0; i < studentNames.Length; i++)
+{
+    Console.WriteLine(studentNames[i]);
+}
---
+++ ConsoleApp.Lists/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Declare a List
+List<int> grades = new List<int>();
+int grade = 0;
+
+// Add values to list
+do
+{
+    Console.WriteLine("Enter Grade: ");
+    grade = Convert.ToInt32(Console.ReadLine());
+    if (grade != -1)
+    {
+        grades.Add(grade);
+    }
+} while (grade != -1);
+
+// Print values in list - for
+Console.WriteLine("Printing names via for loop");
+for (int i = 0; i < grades.Count; i++)
+{
+    Console.WriteLine(grades[i]);
+}
+
+// Print values in list - foreach
+foreach (int g in grades)
+{
+    Console.WriteLine(g);
+}
